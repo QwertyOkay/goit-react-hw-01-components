15032243f65db3ee81a0f3ce618b0edb3c6c6864
Alexandr Kotenko <60352683+QwertyOkay@users.noreply.github.com>
2022-07-03 15:35:54 +0300

    Profile

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Profile from './components/Profile/Profile';
+import user from './data/user.json';
+
+function App() {
+  return ( 
+<>
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    
+    </>
+  );
+}
+
+export default App;
