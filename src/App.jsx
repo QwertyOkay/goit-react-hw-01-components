734683f81f43078b77c72f1c7fbd20cf109f3d93
Alexandr Kotenko <60352683+QwertyOkay@users.noreply.github.com>
2022-07-03 16:24:32 +0300

    other

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
+import Statistics from 'components/Statistics/Statistics'; 
+import statisticalData from './data/data.json' 
+import FriendList from 'components/FriendList/FriendList';
+import friends from './data/friends.json';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import transactions from './data/transactions.json';
 
 function App() {
   return ( 
@@ -11,7 +17,10 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-    
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
